dc91471523b9db1c1df7a42643b8a71872d4d0d7
Stéphane Chouinard <stephane.chouinard@gmail.com>
2015-01-27 16:01:05 -0500

    tentative de payload pour charger les données de départ

--- app/models/killer.js
+++ app/models/killer.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	name: DS.attr('string'),
-	baseCost: DS.attr('number')
+	baseCost: DS.attr('number'),
+	level: DS.attr('number')
 });
--- app/routes/games.js
+++ app/routes/games.js
@@ -13,6 +13,19 @@ export default Ember.Route.extend({
 				}
 			});
 			newgame.save();
+			
+			//declare killers
+			var pushData = {
+				killers : [
+					{id: 1,	name: "Dog", level: 1, baseCost: 10},
+					{id: 2,	name: "Mario", level: 2, baseCost: 100},
+					{id: 3, name: "Link", level: 3, baseCost: 1000}
+				]
+			};
+
+			//push killers in the store (it will update the store if an older version of the game is present)
+			this.store.pushPayload(pushData);
+
 		},
 		deleteGame: function(game){
 			game.deleteRecord();
